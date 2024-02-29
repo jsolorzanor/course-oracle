ae31dff91efa5eeecf252764390b3449f622cb3a
Academico <Academico@T08KETIPC06A202.instructores.senati.local>
2024-02-29 12:47:44 -0500

    sql

---
+++ ejercicio1.sql
@@ -0,0 +1,8 @@
+--Realizar un backup de una base de datos.
+
+--1.-Listar todos los customers que inicien su id con la letra 'A'. Guardar el archivo como ejercicio 1.sql y lo subes a tu supositorio 
+
+SELECT * FROM Customers
+
+WHERE CustomerID LIKE 'A%'
+
