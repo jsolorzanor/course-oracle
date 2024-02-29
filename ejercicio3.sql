95d1c9d5896ce4d01b65275b612c6c4fc1df7526
Academico <Academico@T08KETIPC06A202.instructores.senati.local>
2024-02-29 13:25:19 -0500

    sql

---
+++ ejercicio3.sql
@@ -0,0 +1,5 @@
+--3.-Listar a todos los orders que el campo ShipRegion no sean NULL. Guardar el archivo como "ejercicio03.sql" y lo subes a tu repositorio.
+
+SELECT * FROM Orders
+
+Where ShipRegion IS NOT NULL
