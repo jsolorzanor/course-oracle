5fd925b6d40bcfcebe3112c8d7054059d44c472f
Academico <Academico@T08KETIPC06A202.instructores.senati.local>
2024-02-29 13:18:22 -0500

    sql

---
+++ ejercicio2.sql
@@ -0,0 +1,13 @@
+--2.-Listar todos los customers que sean de los paises: France, Germany, sweden. Guardar el archivo como "ejercicio02-sql" y subes a tu repositorio 
+
+SELECT * FROM Customers
+
+WHERE Country = 'France'
+
+OR
+
+Country = 'Germany'
+
+OR
+
+Country = 'Sweden'
